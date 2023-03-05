1b90868501cc0ea30d3b5db01a248d2d168932a1
Binyang Qin <qbylucky@gmail.com>
2023-03-05 17:21:45 +0800

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,51 +1,103 @@
 export default function AboutPage() {
   return (
-    <div className="mt-10 container lg:max-w-screen-lg mx-auto">
-      <div className="rounded-md bg-white mx-5 lg:mx-0 px-5 py-4 shadow">
-        <h2 className="text-xl font-bold">关于我</h2>
-        <ul className="mt-3 pl-4 space-y-2">
-          <li className="list-disc list-inside indent-1">
-            我是一个在美国的中国人
-          </li>
-          <li className="list-disc list-inside indent-1">
-            我是一个在美国的中国人
-          </li>
-          <li className="list-disc list-inside indent-1">
-            我是一个在美国的中国人
-          </li>
-        </ul>
-      </div>
+    <div className="mt-10 mb-5 container lg:max-w-screen-lg mx-auto">
+      <section className="rounded-md bg-white mx-5 lg:mx-0 px-5 py-4 shadow">
+        <h1 className="text-gray-800 text-xl font-semibold">About me</h1>
+        <p className="mt-3 text-gray-700">
+          我是Hannah Lin，2002年毕业于美国Biola 大学,
+          获得TESOL硕士学位。在中国某大学英语系执教十多年后，2011年与家人一起移民加拿大。定居加拿大后曾在当地大学教授ESL英文，并持续辅导留学生及新老移民的口语及写作。
+        </p>
+        <p className="mt-3 text-gray-700">
+          丰富的英语教学经验，多年海外求学及生活的经历，对东西方文化差异的独到见解，使我能够使用独特的英语教学模式，帮助学生找到最高效的学习方法，在最短时间内掌握原汁原味的实用英语。为了帮助到国内外更多的英语学习者，五年前开始在YouTube上制作并上传优质的英语学习视频。
+        </p>
+      </section>
 
-      <div className="mt-5 rounded-md bg-white mx-5 lg:mx-0 px-5 py-4 shadow">
-        <h2 className="text-xl font-bold">关注我</h2>
-        <ul className="mt-3 pl-4 space-y-2">
+      <section className="mt-5 rounded-md bg-white mx-5 lg:mx-0 px-5 py-4 shadow">
+        <h2 className="text-gray-800 text-xl font-semibold">Credentials</h2>
+        <ul className="mt-3 pl-4 space-y-2 text-gray-700">
           <li className="list-disc list-inside indent-1">
-            <a className="text-blue-500" href="">
-              Twitter
-            </a>
+            MA (Master of Arts) in TESOL (Teaching English to Speakers of Other
+            Language)- Biola University, USA
           </li>
           <li className="list-disc list-inside indent-1">
-            <a className="text-blue-500" href="">
-              微博
-            </a>
+            BA in International Trade and Economics, China
           </li>
         </ul>
-      </div>
+      </section>
 
-      <div className="mt-5 rounded-md bg-white mx-5 lg:mx-0 px-5 py-4 shadow">
-        <h2 className="text-xl font-bold">其他</h2>
-        <ul className="mt-3 pl-4 space-y-2">
+      <section className="mt-5 rounded-md bg-white mx-5 lg:mx-0 px-5 py-4 shadow">
+        <h2 className="text-gray-800 text-xl font-semibold">Teaching history</h2>
+        <ul className="mt-3 pl-4 space-y-2 text-gray-700">
           <li className="list-disc list-inside indent-1">
-            我是一个在美国的中国人
+            ESL instructor and tutor at Trinity Western University in Canada
           </li>
           <li className="list-disc list-inside indent-1">
-            我是一个在美国的中国人
+            Instructor at an Adult Language School in Vancouver
           </li>
           <li className="list-disc list-inside indent-1">
-            我是一个在美国的中国人
+            Private English tutor for international students and Chinese
+            immigrants
+          </li>
+          <li className="list-disc list-inside indent-1">YouTube content creator focused on English learning</li>
+          <li className="list-disc list-inside indent-1">
+            Kids' Sunday School teacher at a local church in Vancouver, Canada
+          </li>
+          <li className="list-disc list-inside indent-1">
+            Associate Professor at a Chinese University with 10 years of
+            teaching experience
+          </li>
+          <li className="list-disc list-inside indent-1">English teacher at an international preschool in China</li>
+        </ul>
+      </section>
+
+      <section className="mt-5 rounded-md bg-white mx-5 lg:mx-0 px-5 py-4 shadow">
+        <h2 className="text-gray-800 text-xl font-semibold">Languages I speak</h2>
+        <ul className="mt-3 pl-4 space-y-2 text-gray-700">
+          <li className="list-disc list-inside indent-1">English</li>
+          <li className="list-disc list-inside indent-1">Chinese</li>
+          <li className="list-disc list-inside indent-1">Korean</li>
+        </ul>
+      </section>
+
+      <section className="mt-5 rounded-md bg-white mx-5 lg:mx-0 px-5 py-4 shadow">
+        <h2 className="text-gray-800 text-xl font-semibold">
+          Teaching fields that I’m passionate about
+        </h2>
+        <ul className="mt-3 pl-4 space-y-2 text-gray-700">
+          <li className="list-disc list-inside indent-1">
+            American English pronunciation 美式英语发音
+          </li>
+          <li className="list-disc list-inside indent-1">
+            IPronunciation correction 纠音
+          </li>
+          <li className="list-disc list-inside indent-1">
+            spoken English/listening skills 英语口语/英语听力
+          </li>
+          <li className="list-disc list-inside indent-1">
+            children’s English 少儿英语
+          </li>
+          <li className="list-disc list-inside indent-1">
+            Medical English 医疗英语
+          </li>
+          <li className="list-disc list-inside indent-1">
+            News English 新闻英语
           </li>
         </ul>
-      </div>
+      </section>
+
+      <section className="mt-5 rounded-md bg-white mx-5 lg:mx-0 px-5 py-4 shadow">
+        <h2 className="text-gray-800 text-xl font-semibold">Family information</h2>
+        <ul className="mt-3 pl-4 space-y-2 text-gray-700">
+          <li className="list-disc list-inside indent-1">
+            A happy wife married to a loving, handy, hardworking husband with
+            one precious son, 17, and one precious daughter, 13.
+          </li>
+          <li className="list-disc list-inside indent-1">
+            幸福的妻子/妈妈。
+            先生爱心洋溢、灵巧能干、勤劳踏实，育有一子17岁，一女13岁。
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
