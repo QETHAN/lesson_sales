2cfac853f3a8fac92514d51c7787c572aec36be2
ethan <qbylucky@gmail.com>
2023-03-16 10:50:18 +0800

    Update checkout api

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -7,7 +7,8 @@ export default async function handler(req, res) {
 
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card", "alipay"],
+      // payment_method_types: ["card", "alipay"],
+      payment_method_types: ["card"],
       line_items: [
         {
           price,
