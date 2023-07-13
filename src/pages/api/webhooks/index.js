cc9169bb98603836d441ae98c19accd080e5b1b4
ethan <qbylucky@gmail.com>
2023-07-13 16:09:41 +0800

    feat: email notify

---
+++ src/pages/api/webhooks/index.js
@@ -0,0 +1,71 @@
+import Cors from "micro-cors";
+
+const cors = Cors({
+  allowMethods: ["POST", "HEAD"],
+});
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const webhookSecret =
+  "whsec_e899a987b1c34cd767c80e6dd43f2d653a941aeb9d91941ad6f295924c3898eb";
+
+// Stripe requires the raw body to construct the event.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+async function buffer(readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
+
+const webhookHandler = async (req, res) => {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const sig = req.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf.toString(),
+        sig,
+        webhookSecret
+      );
+    } catch (err) {
+      // On error, log and return the error message
+      console.log(`❌ Error message: ${err.message}`);
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Successfully constructed event
+    console.log("✅ Success:", event.id);
+
+    // Handle the event
+    switch (event.type) {
+      case "charge.succeeded":
+        const chargeSucceeded = event.data.object;
+        // Then define and call a function to handle the event charge.succeeded
+        const { email, name } = chargeSucceeded.billing_details;
+        const receipt_url = chargeSucceeded.receipt_url;
+
+        console.log("---->", email, name, receipt_url);
+
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    res.send();
+  }
+};
+
+export default cors(webhookHandler);
---
+++ src/server.js
@@ -0,0 +1,52 @@
+// server.js
+//
+// Use this sample code to handle webhook events in your integration.
+//
+// 1) Paste this code into a new file (server.js)
+//
+// 2) Install dependencies
+//   npm install stripe
+//   npm install express
+//
+// 3) Run the server on http://localhost:4242
+//   node server.js
+
+// The library needs to be configured with your account's secret key.
+// Ensure the key is kept out of any version control system you might be using.
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const express = require('express');
+const app = express();
+
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = "whsec_e899a987b1c34cd767c80e6dd43f2d653a941aeb9d91941ad6f295924c3898eb";
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'charge.succeeded':
+      const chargeSucceeded = event.data.object;
+      // Then define and call a function to handle the event charge.succeeded
+      console.log('---->', chargeSucceeded)
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
